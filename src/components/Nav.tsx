0c193f2a5547b3e5ff5f21d5dcd89c146b8047d2
Kurt DS <allenkurtds.dev@gmail.com>
2026-03-26 21:48:16 +0800

    feat: initialized tailwind configurations and setup components

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav>
+      <h1>Atlas Brain</h1>
+      <div>
+        <a href="#">Github</a>
+        <a href="#">Sign up</a>
+      </div>
+    </nav>
+  );
+}
