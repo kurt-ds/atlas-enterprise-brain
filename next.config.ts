6362a292f53844bdc793081e100043261e9b959b
kurt-ds <allenkurtds.dev@gmail.com>
2026-04-12 19:46:17 +0800

    debug: fix status 500 on vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["@huggingface/transformers", "onnxruntime-node"],
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb", // Adjust based on your PDF sizes
